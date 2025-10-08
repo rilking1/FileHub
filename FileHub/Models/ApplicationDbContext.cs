8ec76ff0928ed892130c5fe3986b626057be9dd5
rilking1 <kozakevycr2@gmail.com>
2025-10-08 20:14:51 +0300

    Добавьте файлы проекта.

---
+++ FileHub/Models/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace FileHub.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<FileRecord> Files { get; set; }
+    }
+}
---
+++ FileHub/Models/FileRecord.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace FileHub.Models
+{
+    public class FileRecord
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string FileName { get; set; }
+
+        public string OriginalName { get; set; }
+        public string Path { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public DateTime ModifiedAt { get; set; } = DateTime.Now;
+
+        public string UploadedBy { get; set; }
+        public string EditedBy { get; set; }
+    }
+}
